79ed9b3df2a4816dabb8a12cc31509b362f31ecf
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-02-07 17:56:51 +0000

    Finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+
+  // Calculate temperature in celsius and round to 1 decimal place
+  const Celsius = Math.round(((degrees - 32) * 5/9) * 10) / 10;
+
+  // Return celsius
+  return Celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+
+  // Calculate temperature in farenheit and round to 1 decimal place
+  const Farenheit = Math.round(((degrees * 9/5) + 32) * 10) / 10;
+
+  // Return farenheit
+  return Farenheit;
 };
 
 // Do not edit below this line
