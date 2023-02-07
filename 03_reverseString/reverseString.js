ba40efa7330b3fdf906bd703e17f46faabfea71e
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-02-07 16:23:11 +0000

    Finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // Create empty array to append each letter of given string to
+    const arr = [];
 
+    // Loop through each letter in given string
+    for (i = 0; i < str.length; i++) {
+
+        // Add given letter i to the beginning of the array
+        arr.unshift(str[i]);
+    }
+
+    // Create new variable to add the letters from the array to without spaces between them and return the newString
+    newString = arr.join("");
+    return newString;
 };
 
 // Do not edit below this line
