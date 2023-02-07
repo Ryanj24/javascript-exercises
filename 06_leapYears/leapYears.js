960dba4929f9a340b3e0290ec826a42b264698ff
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-02-07 17:44:55 +0000

    Finished exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,33 @@
-const leapYears = function() {
+/// A year is a leap year if:
+        // 1. Its divisible by 4
+        // 2. If it is divisible by 100 and 400
 
+
+const leapYears = function(year) {
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            return true;
+        }
+    } else if (year % 4 == 0) {
+        return true;
+    }
+    return false;
 };
+
+
+
+
+
+
+/*
+if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0))
+    {
+        return true;
+    }
+    return false;
+
+
+*/
 
 // Do not edit below this line
 module.exports = leapYears;
